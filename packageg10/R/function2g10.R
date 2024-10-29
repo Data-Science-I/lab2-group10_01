737de591d9a9046d4dc3b84e6dba898169f34e6a
Lejing Li <lel4011@med.cornell.edu>
2024-10-29 18:20:44 -0400

    change function 2

--- packageg10/R/function2g10.R
+++ packageg10/R/function2g10.R
@@ -1,16 +1,16 @@
 calculate_drg_statistics <- function(data, stat_type = c("mean", "median", "sd")) {
-
   # Check that the stat_type is valid
   stat_type <- match.arg(stat_type)
 
-  # Extract the column for Average Medicare Payments
-  payment_data <- data$Average.Medicare.Payments
-
-  # Calculate the chosen statistic
-  result <- switch(stat_type,
-                   "mean" = mean(payment_data, na.rm = TRUE),
-                   "median" = median(payment_data, na.rm = TRUE),
-                   "sd" = sd(payment_data, na.rm = TRUE))
+  # Calculate the chosen statistic for each DRG Definition group
+  result <- data %>%
+    group_by(DRG.Definition) %>%  # Group by the 'DRG Definition' column
+    summarize(statistic = switch(stat_type,
+                                 "mean" = mean(Average.Medicare.Payments, na.rm = TRUE),
+                                 "median" = median(Average.Medicare.Payments, na.rm = TRUE),
+                                 "sd" = sd(Average.Medicare.Payments, na.rm = TRUE))) %>%
+    rename_with(~ paste(stat_type, "payment", sep = "_"), statistic)  # Rename the column to include the stat_type prefix
 
   return(result)
+
 }
